22a448259bde38f0e953711c26eb6f45ca5d381c
volkan.ceylan <volkanceylan@gmail.com>
2016-05-19 16:10:16 +0300

    added genre table migration

---
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20160519_154700_GenreTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace MovieTutorial.Migrations.DefaultDB
+{
+    [Migration(20160519154700)]
+    public class DefaultDB_20160519_154700_GenreTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Genre").InSchema("mov")
+                .WithColumn("GenreId").AsInt32().NotNullable()
+                    .PrimaryKey().Identity()
+                .WithColumn("Name").AsString(100).NotNullable();
+
+            Alter.Table("Movie").InSchema("mov")
+                .AddColumn("GenreId").AsInt32().Nullable()
+                    .ForeignKey("FK_Movie_GenreId", "mov", "Movie", "MovieId");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
