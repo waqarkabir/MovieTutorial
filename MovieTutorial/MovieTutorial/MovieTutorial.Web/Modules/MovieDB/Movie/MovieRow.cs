c0c4c4ed3be3f95f3328284efa105b5b240a78af
volkan.ceylan <volkanceylan@gmail.com>
2016-05-19 19:26:52 +0300

    genre inplaceadd functionality

--- MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Movie/MovieRow.cs
+++ MovieTutorial/MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Movie/MovieRow.cs
@@ -72,7 +72,7 @@ namespace MovieTutorial.MovieDB.Entities
         }
 
         [DisplayName("Genre"), ForeignKey("[mov].Genre", "GenreId"), LeftJoin("g")]
-        [LookupEditor(typeof(GenreRow))]
+        [LookupEditor(typeof(GenreRow), InplaceAdd = true)]
         public Int32? GenreId
         {
             get { return Fields.GenreId[this]; }
