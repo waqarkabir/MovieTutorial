b523207c3b2d917f3aeb4bd0918e9ca4a339b854
waqarkabir <waqarkabir10@gmail.com>
2020-09-06 15:05:46 +0500

    Update GenreForm.cs
    Deleted extra usings.

--- MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Genre/GenreForm.cs
+++ MovieTutorial/MovieTutorial.Web/Modules/MovieDB/Genre/GenreForm.cs
@@ -3,10 +3,8 @@ namespace MovieTutorial.MovieDB.Forms
 {
     using Serenity;
     using Serenity.ComponentModel;
-    using Serenity.Data;
     using System;
     using System.ComponentModel;
-    using System.Collections.Generic;
     using System.IO;
 
     [FormScript("MovieDB.Genre")]
